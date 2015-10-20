db4a55702eda21f43c34193ebb5c1357325e8a2a
Vasily Kirichenko <vaskir@gmail.com>
2015-10-20 23:29:10 +0300

    it works (in VS 2015 FSI)

--- InfersTest/Script.fsx
+++ InfersTest/Script.fsx
@@ -7,23 +7,27 @@ type IShowTester =
     inherit ITestable
     abstract Run : unit -> unit
 
+type R<'a> = { S: string; A: 'a list }
+let r = { S = "foo"; A = [[| Some 1; None |]] }
+
 let printfTest = 
     {
         new IShowTester with
-            member x.Run() = sprintf "%A" (Some 1) |> ignore
-            member x.Fini() = ()
-            member x.Init() = ()
+            member __.Run() = 
+                for _ in 1..100 do sprintf "%A" v |> ignore
+            member __.Fini() = ()
+            member __.Init() = ()
             member __.Name = "printf"
     }
 
 let infersTest = 
     {
         new IShowTester with
-            member x.Run() = show (Some 1) |> ignore
-            member x.Fini() = ()
-            member x.Init() = ()
+            member __.Run() = for _ in 1..100 do show v |> ignore
+            member __.Fini() = ()
+            member __.Init() = ()
             member __.Name = "infers"
     }
 
-let tester = ImplementationComparer<IShowTester>(printfTest, [infersTest])
-tester.Run (repeat 100 (fun x -> x.Run()), id = "test 0")
+let tester = ImplementationComparer<IShowTester>(infersTest, [printfTest], verbose = true, warmup = true)
+tester.Run (repeat 10 (fun x -> x.Run()), id = "test")
