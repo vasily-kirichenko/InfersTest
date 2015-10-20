fb0b1f3678c6b058fa6758d347b8d30195237823
Vasily Kirichenko <vaskir@gmail.com>
2015-10-20 21:06:07 +0300

    init

---
+++ InfersTest/Script.fsx
@@ -0,0 +1,29 @@
+﻿#load "Scripts/load-references.fsx"
+
+open PerfUtil
+open Infers.Toys.Pretty
+
+type IShowTester =
+    inherit ITestable
+    abstract Run : unit -> unit
+
+let printfTest = 
+    {
+        new IShowTester with
+            member x.Run() = sprintf "%A" (Some 1) |> ignore
+            member x.Fini() = ()
+            member x.Init() = ()
+            member __.Name = "printf"
+    }
+
+let infersTest = 
+    {
+        new IShowTester with
+            member x.Run() = show (Some 1) |> ignore
+            member x.Fini() = ()
+            member x.Init() = ()
+            member __.Name = "infers"
+    }
+
+let tester = ImplementationComparer<IShowTester>(printfTest, [infersTest])
+tester.Run (repeat 100 (fun x -> x.Run()), id = "test 0")
---
+++ InfersTest/Scripts/load-references.fsx
@@ -0,0 +1,13 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../packages/PerfUtil/lib/net40/PerfUtil.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
+#r "../../packages/PPrint/lib/net45/PPrint.dll"
+#r "../../../Infers/Libs/Infers/bin/Release/Infers.Core.dll"
+#r "../../../Infers/Libs/Infers/bin/Release/Infers.dll"
+#r "../../../Infers/Libs/Infers.Rep/bin/Release/Infers.Rep.dll"
+#r "../../../Infers/Libs/Infers.Toys/bin/Release/Infers.Toys.dll"
+#r "../../packages/FParsec/lib/net40-client/FParsecCS.dll"
+#r "../../packages/FParsec/lib/net40-client/FParsec.dll"
